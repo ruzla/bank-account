9cd1c2c36ff692394d320a600cd020f3f6a2515e
Russell <russelldoe@hotmail.co.uk>
2019-03-04 21:12:11 +0000

    added some prototypes and tests

--- __tests__/bank.test.js
+++ __tests__/bank.test.js
@@ -8,18 +8,52 @@ describe('Constructor', () => {
 
 
 describe('Deposit', () => {
-
+    const currentAccount = new Account('Russell');
+    currentAccount.deposit(100);
+    
+    it('array to contain one deposits', () => {
+     expect(currentAccount.deposits).toHaveLength(1);
+    });
+    
+    it('array to contain 100', () => {
+        expect(currentAccount.deposits[0]).toEqual(100);
+    });
 });
 
 
 describe('Withdraw', () => {
+    const currentAccount = new Account('Russell');
+    currentAccount.withdraw(-100);
+    
+    it('expect array to contain one withdrawal', () => {
+        expect(currentAccount.withdrawls).toHaveLength(1);
+    });
 
+    it('array to contain -100', () => {
+        expect(currentAccount.withdrawls[0]).toEqual(-100);
+    });
 });
 
 describe('Check current balance', () => {
-
+    const currentAccount = new Account('Russell');
+    currentAccount.deposit(100);
+    currentAccount.withdraw(-100);
+    currentAccount.deposit(50);
+    
+    it('the balance should be 50', () => {
+        expect(currentAccount.checkBalance()).toBe(50);
+    });
 });
 
 describe('View a list of transactions', () => {
+    const currentAccount = new Account('Russell');
+    currentAccount.deposit(100);
+    currentAccount.withdraw(-100);
+    currentAccount.deposit(50);
+
+    it('lists transactions', () => {
+        expect(currentAccount.viewTransactions()).toEqual([100, -100, 50]);
+    })
+
 
 });
--- src/bank.js
+++ src/bank.js
@@ -1,13 +1,28 @@
 function Account(name) {
-    this.name = name;
+    this.accountHolder = name;
     this.deposits = [];
     this.withdrawls = [];
-    this.transactions = [];
-    this.balance = 0;
+    this.balance = [];
 };
 
+Account.prototype.deposit = function(ammount) {
+    this.deposits.push(ammount);
+    this.balance.push(ammount);
+};
 
+Account.prototype.withdraw = function(ammount) {
+    this.withdrawls.push(ammount);
+    this.balance.push(ammount);
+};
 
+Account.prototype.checkBalance = function() {
+     return this.balance.reduce((acc, current) => (acc += current), 0);
+
+};
+
+Account.prototype.viewTransactions = function() {
+    return this.balance;
+};
 
 
 module.exports = {
