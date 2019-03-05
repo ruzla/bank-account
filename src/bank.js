b91f500eb7d654f7d89f5feb12fe570e6ff27978
Russell <russelldoe@hotmail.co.uk>
2019-03-05 06:19:50 +0000

    added transactions to account constructor

--- src/bank.js
+++ src/bank.js
@@ -2,26 +2,29 @@ function Account(name) {
     this.accountHolder = name;
     this.deposits = [];
     this.withdrawls = [];
-    this.balance = [];
+    this.transactions = [];
+    this.balance = 0;
 };
 
 Account.prototype.deposit = function(ammount) {
     this.deposits.push(ammount);
-    this.balance.push(ammount);
+    this.transactions.push(ammount);
+    this.balance += ammount;
 };
 
 Account.prototype.withdraw = function(ammount) {
     this.withdrawls.push(ammount);
-    this.balance.push(ammount);
+    this.transactions.push(ammount);
+    this.balance += ammount;
 };
 
 Account.prototype.checkBalance = function() {
-     return this.balance.reduce((acc, current) => (acc += current), 0);
-
+    //  return this.balance.reduce((acc, current) => (acc += current), 0);
+    return this.balance;
 };
 
 Account.prototype.viewTransactions = function() {
-    return this.balance;
+    return this.transactions;
 };
 
 
